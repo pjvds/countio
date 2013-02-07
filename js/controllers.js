9dfe25b3b8f975c2fdd7a282cff1fb9a012bd8c8
Pieter Joost van de Sande <pj@born2code.net>
2013-02-07 22:45:19 +0100

    Bootstrapify index.html

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,11 @@
 function TrackCtrl($scope) {
+	$scope.active = { 'name' : 'beer', 'count' : '5', 'counts' : [
+		  	{ 'date' : '2012-03-5T23:51:32.031Z', 'inc' : '1' },
+		  	{ 'date' : '2012-03-4T07:12:11.031Z', 'inc' : '1' },
+		  	{ 'date' : '2012-03-3T22:00:44.031Z', 'inc' : '1' },
+		  	{ 'date' : '2012-03-2T20:39:07.031Z', 'inc' : '1' },
+		  	{ 'date' : '2012-03-1T08:05:22.031Z', 'inc' : '1' }
+		] };
 	$scope.tracks = [
 		{ 'name' : 'beer', 'count' : '5', 'counts' : [
 		  	{ 'date' : '2012-03-5T23:51:32.031Z', 'inc' : '1' },
@@ -12,6 +19,5 @@ function TrackCtrl($scope) {
 		  	{ 'date' : '2012-03-4T07:12:11.031Z', 'inc' : '1' },
 		  	{ 'date' : '2012-03-3T22:00:44.031Z', 'inc' : '1' },
 		  	{ 'date' : '2012-03-2T20:39:07.031Z', 'inc' : '1' }
-		] }
-	];
+		 ]}];
 }
