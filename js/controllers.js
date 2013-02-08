575751c61f1310af90b390ae9e7f71539c40402f
Pieter Joost van de Sande <pj@born2code.net>
2013-02-08 11:35:59 -0500

    Refactor model towards period

--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,20 @@
 function TrackCtrl($scope) {
-	$scope.track = { 'name' : 'beer', 'count' : 5, 'counts' : [
-		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-1T08:05:22.031Z'), 'inc' : 1 }
-	] };
+	$scope.track = {
+        'name' : 'beer',
+        'currentPeriod' : {
+            'name' : 'todays',
+            'count' : 5
+        }};
 	
 	$scope.count = function(inc) {
 		var track = $scope.track;
-		track.count += inc;
-        
-        track.counts[0].date = new Date();
-		track.counts.push({'date' : new Date(), 'inc' : inc});
+		track.currentPeriod.count += inc;
 	}
     
-    $scope.flipCharacters = function(count) {
+    $scope.flipCharacters = function(track) {
         var chars = [];
         
+        var count = track.currentPeriod.count;
         var countString = count.toString();
         var len = countString.length;
 
