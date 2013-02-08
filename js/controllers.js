c6079933b6da68245c167a04c73c2e1d81a96620
Pieter Joost van de Sande <pj@born2code.net>
2013-02-08 05:47:21 -0500

    Add spinner counter

--- js/controllers.js
+++ js/controllers.js
@@ -1,29 +1,48 @@
 function TrackCtrl($scope) {
 	$scope.active = { 'name' : 'beer', 'count' : 5, 'counts' : [
-		  	{ 'date' : '2012-03-5T23:51:32.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-4T07:12:11.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-3T22:00:44.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-2T20:39:07.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-1T08:05:22.031Z', 'inc' : 1 }
+		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-1T08:05:22.031Z'), 'inc' : 1 }
 	] };
 	$scope.tracks = [
 		{ 'name' : 'beer', 'count' : 5, 'counts' : [
-		  	{ 'date' : '2012-03-5T23:51:32.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-4T07:12:11.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-3T22:00:44.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-2T20:39:07.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-1T08:05:22.031Z', 'inc' : 1 }
+		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-1T08:05:22.031Z'), 'inc' : 1 }
 	] },
 		{ 'name' : 'cofee', 'count' : 4, 'counts' : [
-		  	{ 'date' : '2012-03-5T23:51:32.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-4T07:12:11.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-3T22:00:44.031Z', 'inc' : 1 },
-		  	{ 'date' : '2012-03-2T20:39:07.031Z', 'inc' : 1 }
+		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
+		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 }
 		 ]}];
 	
 	$scope.count = function(inc) {
 		var active = $scope.active;
 		active.count += inc;
-		active.counts.push({'data' : new Date(), 'inc' : inc});
+        
+        active.counts[0].date = new Date();
+		active.counts.push({'date' : new Date(), 'inc' : inc});
+	}
+    
+    $scope.flipCharacters = function(count) {
+        var chars = [];
+        
+        var countString = count.toString();
+        var len = countString.length;
+
+        for (var i = 0; i < 3-len; i++){
+            chars.push('0');
+        }
+
+        for (var i = 0; i < len; i++) {
+            chars.push(countString[i]);
+        }
+        
+        return chars;
 	}
 }
