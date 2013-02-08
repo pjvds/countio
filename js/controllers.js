528e6490123093e81092887a8765722cba72fb26
Pieter Joost van de Sande <pj@born2code.net>
2013-02-08 11:21:44 -0500

    Switch to single track in model

--- js/controllers.js
+++ js/controllers.js
@@ -1,32 +1,18 @@
 function TrackCtrl($scope) {
-	$scope.active = { 'name' : 'beer', 'count' : 5, 'counts' : [
+	$scope.track = { 'name' : 'beer', 'count' : 5, 'counts' : [
 		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
 		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
 		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
 		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 },
 		  	{ 'date' : new Date('2012-03-1T08:05:22.031Z'), 'inc' : 1 }
 	] };
-	$scope.tracks = [
-		{ 'name' : 'beer', 'count' : 5, 'counts' : [
-		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-1T08:05:22.031Z'), 'inc' : 1 }
-	] },
-		{ 'name' : 'cofee', 'count' : 4, 'counts' : [
-		  	{ 'date' : new Date('2012-03-5T23:51:32.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-4T07:12:11.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-3T22:00:44.031Z'), 'inc' : 1 },
-		  	{ 'date' : new Date('2012-03-2T20:39:07.031Z'), 'inc' : 1 }
-		 ]}];
 	
 	$scope.count = function(inc) {
-		var active = $scope.active;
-		active.count += inc;
+		var track = $scope.track;
+		track.count += inc;
         
-        active.counts[0].date = new Date();
-		active.counts.push({'date' : new Date(), 'inc' : inc});
+        track.counts[0].date = new Date();
+		track.counts.push({'date' : new Date(), 'inc' : inc});
 	}
     
     $scope.flipCharacters = function(count) {
