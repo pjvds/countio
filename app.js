82ddae9c411af3f61c3f755ee4d237f4b1c0ba48
Pieter Joost van de Sande <pj@born2code.net>
2013-02-10 13:32:27 +0100

    Add API functions post and list

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ var host = process.env.IP || 'localhost';
 // Database
 mongoose.connect('mongodb://countio:CountioDatabasePassword@linus.mongohq.com:10025/countio');
 
+// Configure schema's
+var Schema = mongoose.Schema;
+
+var Track = new Schema({  
+  title: { type: String, required: true },  
+  description: { type: String, required: true },
+  modified: { type: Date, default: Date.now }
+});
+
+var TrackModel = mongoose.model('Track', Track);
+
 // Config
 app.configure(function () {
   app.use(express.bodyParser());
@@ -19,9 +30,36 @@ app.configure(function () {
   app.use(app.router);
   app.use(express.static(path.join(application_root, "public")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  //app.use(express.session({secret: "HumansAreNotBrokenByDefault" }));
 });
 
-app.get('/status', function (req, res) {
+app.get('/api/v1/', function(req, res) {
+  res.send('count.io API is running');
+});
+
+app.post('/api/v1/track', function (req, res) {
+	var track;
+	console.log("POST: ");
+	console.log(req.body);
+
+	track = new TrackModel({
+    title: req.body.title,
+    description: req.body.description,
+    style: req.body.style,
+  });
+});
+
+app.get('/api/v1/tracks', function(req, res) {
+	return TrackModel.find(function(err, tracks) {
+		if (!err) {
+      return res.send(tracks);
+    } else {
+      return console.log(err);
+    }
+	});
+});
+
+app.get('/track', function (req, res) {
   res.send('count.io API is running');
 });
 
